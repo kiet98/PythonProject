725e9b84f93918c914d7df324a64d5ef25fb123a
kiet98 <vovankiet98@gmail.com>
2024-02-23 19:34:48 +0700

    demo selenium

---
+++ BrowserController.py
@@ -0,0 +1,8 @@
+import logging as log
+from selenium import webdriver as WebDriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
--- main.py
+++ main.py
@@ -1,17 +1,53 @@
-# This is a sample Python script.
+from selenium import webdriver as WebDriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import BrowserController
+
+formatProductLink = ("https://www.etsy.com/listing/{product_id}/black-and-beige-set-of-3-prints-henri?click_key"
+                     "=c710384ecc86679044eb9780719f001c8e2654f3%3A1446293575&click_sum=19d4d1fa&ref"
+                     "=shop_home_active_1&crt=1")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def setUpChromeWebDriver():
+    driver_manager = ChromeDriverManager()
+    options = Options()
+    service = Service(driver_manager.install())
+    WebDriver.ChromeService = driver_manager.install()
+    options.add_experimental_option("detach", True)
+    return WebDriver.Chrome(service=service, options=options)
 
 
-# Press the green button in the gutter to run the script.
+def openProductLink(product_id: str):
+    print(formatProductLink.format(product_id=product_id))
+    driver.get(formatProductLink.format(product_id=product_id))
+
+
+def findProductName():
+    elementProductName = driver.find_element(by=By.CSS_SELECTOR, value="#listing-page-cart > div.wt-mb-xs-1 > h1")
+    return elementProductName.text
+
+
+def findExploreRelatedSearches():
+    elements = driver.find_elements(by=By.CSS_SELECTOR, value="#content > div.content-wrap.listing-page-content > "
+                                                              "div.wt-body-max-width.wt-mb-xs-6.wt-pl-md-4.wt-pr-md-4"
+                                                              ".wt-pl-lg-5.wt-pr-lg-5 > div.recs-appears-logger > div "
+                                                              "> "
+                                                              "div.tags-section-container.tag-cards-section-container"
+                                                              "-with-images > ul li")
+    for element in elements:
+        e = element.find_element(by=By.CSS_SELECTOR, value="a > h3")
+        print(e.text)
+
+
+driver = None
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-# the first change
+    driver = setUpChromeWebDriver()
+    openProductLink(product_id="1436406443")
+    productName = findProductName()
+    print(f"Product Name: {productName}")
+    findExploreRelatedSearches()
+    driver.quit()
